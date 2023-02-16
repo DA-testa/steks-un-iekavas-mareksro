58a2a348a2f403333b0f8a73f12ac71e055c48e3
Mareks Rozenblats <118176164+mareksro@users.noreply.github.com>
2023-02-17 00:08:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,17 +14,34 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next,i+1))
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if len(opening_brackets_stack) == 0 or not are_matching (opening_brackets_stack[-1].char, next):
+                return i+1
+            opening_brackets_stack.pop()
+            
+    if opening_brackets_stack:
+        return opening_brackets_stack[-1].position
+    return "Success"
+pass
 
 
 def main():
-    text = input()
+    izvele = input("Ievadiet F vai I: ")
+    if izvele == "F":
+        fails = input("Ievadiet faila nosaukumu: ")
+        with open(fails, "r") as f:
+            text = f.read().strip()
+    elif izvele == "I":
+        text = input("Ievadiet iekavas: ")
+    else:
+        print("Invalid choice")
+        return
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    print(mismatch)
 
 
 if __name__ == "__main__":
