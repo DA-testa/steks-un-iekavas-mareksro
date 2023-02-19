66182a262ec681bb53458eef54e84d1503e99a09
Mareks Rozenblats <118176164+mareksro@users.noreply.github.com>
2023-02-20 00:11:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):        
         if next in "([{":
+            
+            pass
             opening_brackets_stack.append(Bracket(next,i+1))
            
 
         if next in ")]}":
-           
+            
+            pass
             if len(opening_brackets_stack) == 0 or not are_matching(opening_brackets_stack[-1].char, next)               
                 return i+1            
             opening_brackets_stack.pop()
@@ -33,12 +36,14 @@ def main():
     text = input()
     mismatch = find_mismatch(text)
     izvele = input("Ievadiet F vai I: ")
+    
     if izvele == "F":
         fails = input("Ievadiet faila nosaukumu: ")
         with open(fails, "r") as f:
             text = f.read().strip()
             mismatch = find_mismatch(text)
             print(mismatch)  
+            
     elif izvele == "I":
         text = input("Ievadiet iekavas: ")
         mismatch = find_mismatch(text)
