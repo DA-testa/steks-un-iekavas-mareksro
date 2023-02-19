a0e4c1ec1b26ee6d2929872e683805d3adc0f40c
Mareks Rozenblats <118176164+mareksro@users.noreply.github.com>
2023-02-19 22:20:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def find_mismatch(text):
             opening_brackets_stack.pop()
             
     if opening_brackets_stack:
-        return opening_brackets_stack[-1].position
+        return opening_brackets_stack[0].position +1
     else :
         return "Success"
 
