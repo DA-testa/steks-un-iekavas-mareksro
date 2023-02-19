dd25f9f222a2176f622325694520143b03ba70bc
Mareks Rozenblats <118176164+mareksro@users.noreply.github.com>
2023-02-20 00:13:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def find_mismatch(text):
         if next in ")]}":
             
             pass
-            if len(opening_brackets_stack) == 0 or not are_matching(opening_brackets_stack[-1].char, next)               
+            if len(opening_brackets_stack) == 0 or not are_matching(opening_brackets_stack[-1].char, next):               
                 return i+1            
             opening_brackets_stack.pop()
             
