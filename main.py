b52c925b3f1bfdaa20daebc442032125502d0ef4
Mareks Rozenblats <118176164+mareksro@users.noreply.github.com>
2023-02-20 00:06:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,34 +12,35 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
+    for i, next in enumerate(text):        
         if next in "([{":
             opening_brackets_stack.append(Bracket(next,i+1))
+           
 
         if next in ")]}":
-            if len(opening_brackets_stack) == 0 or not are_matching(opening_brackets_stack[-1].char, next):
-                return i+1
+           
+            if len(opening_brackets_stack) == 0 or not are_matching(opening_brackets_stack[-1].char, next)               
+                return i+1            
             opening_brackets_stack.pop()
             
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
-    else :
-        return "Success"
+    return "Success"
 
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
-    izvele = input("Ievadiet F vai I:\n")
+    izvele = input("Ievadiet F vai I: ")
     if izvele == "F":
-        fails = input("Ievadiet faila nosaukumu:\n")
+        fails = input("Ievadiet faila nosaukumu: ")
         with open(fails, "r") as f:
             text = f.read().strip()
             mismatch = find_mismatch(text)
             print(mismatch)  
     elif izvele == "I":
-        text = input("Ievadiet iekavas:\n")
+        text = input("Ievadiet iekavas: ")
         mismatch = find_mismatch(text)
         print(mismatch)
         
